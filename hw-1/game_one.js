6503517af48a62946515412266a7142ab73c6e48
kolesova1am <kolalina16@gmail.com>
2024-02-19 16:11:53 +0400

    game one

---
+++ hw-1/game_one.js
@@ -0,0 +1,15 @@
+function monthNumber() {
+    if (month >= 3 && month <= 5) {
+        return 'Весна';
+    } else if (month >= 6 && month <= 8) {
+        return 'Лето';
+    } else if (month >= 9 && month <= 11) {
+        return 'Осень';
+    } else if (month >= 1 && month <= 2 || month === 12){
+        return 'Зима';
+    } else {
+        return 'Ошибка'
+    }
+}
+
+console.log(monthNumber(prompt("Введите число:")));
