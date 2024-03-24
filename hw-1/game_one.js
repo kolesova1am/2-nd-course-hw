647c0c28ffc70b23acf8d5e3479fd3c3d7d7c083
kolesova1am <kolalina16@gmail.com>
2024-03-24 18:26:02 +0400

    Исправление ввода числа 12

--- hw-1/game_one.js
+++ hw-1/game_one.js
@@ -1,12 +1,12 @@
 function monthNumber() {
-const month = prompt("Введите число;" )
+const month = Number(prompt("Введите число;" ))
     if (month >= 3 && month <= 5) {
         alert('Весна');
     } else if (month >= 6 && month <= 8) {
         alert('Лето');
     } else if (month >= 9 && month <= 11) {
         alert('Осень');
-    } else if (month >= 1 && month <= 2 || month === 12){
+    } else if ( month >= 1 && month <= 2 || month === 12){
         alert('Зима');
     } else {
         alert('Ошибка');
